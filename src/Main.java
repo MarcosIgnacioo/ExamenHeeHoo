e1991d60a506d3c3d2f5312121ab375e4f5dd16f
Marcos <iMxyrx@hotmail.com>
2023-03-21 22:47:14 -0700

    Poner el codigo base del profe

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("ASDASDASDSADASD");
-        System.out.println("Wep");
-        int popo = 0;
-        int hola = 1;
+       Ventana1Prueba popo = new Ventana1Prueba();
     }
 }
